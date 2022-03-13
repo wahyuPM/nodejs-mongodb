9752f0a5f5b6d98ee2a143164f04cee090baa432
wahyuPM <wahyupratama191@gmail.com>
2022-03-13 17:29:21 +0800

    add : create pruduct & fetchall data product

--- util/database.js
+++ util/database.js
@@ -1,15 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://wahyuPratama191:Ba929752@cluster0.mickw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  // conecting to mongodb
+  MongoClient.connect('mongodb+srv://wahyuPratama191:Ba929752@cluster0.mickw.mongodb.net/shop?retryWrites=true&w=majority')
     .then(client => {
       console.log('connected to database');
-      callback(client);
+      _db = client.db(); // storing connection to database
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
